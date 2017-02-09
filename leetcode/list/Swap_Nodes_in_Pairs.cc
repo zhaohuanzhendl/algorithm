437565f6ab59af2476d0cb40eff29b7f424b5382
zhz <zhzcsp@gmail.com>
2017-02-09 07:47:46 -0800

    add new cc

---
+++ leetcode/list/Swap_Nodes_in_Pairs.cc
@@ -0,0 +1,42 @@
+/*
+ * Filename   swap_nodes.cc
+ * CreateTime 2017-02-09 06:31:31
+ * 
+ * Copyright  (C) Zhao Huanzhen
+ * Copyright  (C) zhzcsp@gmail.com
+ */
+
+#include <iostream>
+using namespace std;
+
+
+struct ListNode {
+    int         val;
+    ListNode   *next;
+    ListNode(int val) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    ListNode *swap_node(ListNode *head)
+    {
+        if (head == nullptr || head->next == nullptr) {
+            return head;
+        }
+
+        ListNode dummy(-1);
+        dummy.next = head;
+
+        for (ListNode *prev = &dummy, *cur = prev->next, next = cur->next;
+                next;
+                prev = cur, cur = cur->next, next = cur ? cur->next : nullptr) {
+
+            prev->next = next;
+            cur->next = next->next;
+            next->next = cur;
+        
+        }
+        
+        return dummy.next;
+    }
+};
