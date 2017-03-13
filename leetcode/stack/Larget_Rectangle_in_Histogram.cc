b5950598e3da00781d09ed2a30160a50833cc62f
zhz <zhzcsp@gmail.com>
2017-03-13 16:57:22 -0700

    add new cc

---
+++ leetcode/stack/Larget_Rectangle_in_Histogram.cc
@@ -0,0 +1,16 @@
+/*
+ * Filename   Larget_Rectangle_in_Histogram.cc
+ * CreateTime 2017-03-13 04:14:48
+ * 
+ * Copyright  (C) Zhao Huanzhen
+ * Copyright  (C) zhzcsp@gmail.com
+ */
+
+/*
+ * ref from : http://www.cnblogs.com/felixfang/p/3676193.html
+ */
+
+#include <iostream>
+using namespace std;
+
+
