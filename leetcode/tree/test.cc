5db1be6e58a8d844c7157158c92f603013a8fcbc
zhao huanzhen <zhz@zhaodeMBP.lan>
2017-03-18 23:22:19 +0800

    test

---
+++ leetcode/tree/test.cc
@@ -0,0 +1,14 @@
+/*
+ * Filename   test.cc
+ * CreateTime 2017-03-18 23:21:11
+ * 
+ * Copyright  (C) Zhao Huanzhen
+ * Copyright  (C) zhzcsp@gmail.com
+ */
+
+#include <iostream>
+using namespace std;
+
+
+//test
+
