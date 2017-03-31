bbd348614bf11a9d0840c19c51079f5f193dfdc4
zhz <zhzcsp@gmail.com>
2017-03-30 19:30:32 -0700

    update 2d matrix

--- leetcode/search/Search_a_2D_Matrix.cc
+++ leetcode/search/Search_a_2D_Matrix.cc
@@ -37,3 +37,33 @@ public:
         return false;
     }
 };
+
+class Solution_B {
+public:
+    bool searchMatrix(const vector<vector<int> > &matrix, int target)
+    {
+        if (matrix.emptt()) return false;
+        const size_t m = matrix.size();
+        //const size_t n = matrix[0].size();
+        const size_t n = matrix.front().size();
+
+        int first = 0;
+        int last = m * n;
+
+        while (first != last) {
+            int mid = first + (last - first) / 2;
+            int value =  matrix[mid / n ][mid % n];
+            
+            if (value == target) {
+                return true;
+            } else if (value < target) {
+                first = mid + 1; 
+            } else {
+                last = mid;
+            }
+        
+        }
+
+        return false;
+    }
+};
